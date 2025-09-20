2111db5cecf07836b7b61773804c5049586886de
Leema-143 <crajalakshmi3066@gmail.com>
2025-09-20 19:54:26 +0530

    Add files via upload

---
+++ for loop.java
@@ -0,0 +1,11 @@
+public class main{
+public static void main(string[]args)
+{
+    for(int i=1;i<=10;i++){
+        if(1==5){
+            continue;
+        }
+        System.out.println("i");
+    }
+}
+}
